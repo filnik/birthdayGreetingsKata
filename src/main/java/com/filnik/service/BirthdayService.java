9d26588e4ccabfc0b3ad117d3cb25d19f5fa2360
filippo <filippo.depretto@xpeppers.com>
2017-09-13 12:48:07 +0200

    Setup up of the BirthdayServiceTest

--- src/main/java/com/filnik/service/BirthdayService.java
+++ src/main/java/com/filnik/service/BirthdayService.java
@@ -5,8 +5,12 @@ import com.filnik.repository.EmployeeRepository;
 import java.time.LocalDateTime;
 
 public class BirthdayService {
-    public BirthdayService(EmployeeRepository employeeRepository, EmailService emailService) {
+    private final EmployeeRepository employeeRepository;
+    private final EmailService emailService;
 
+    public BirthdayService(EmployeeRepository employeeRepository, EmailService emailService) {
+        this.employeeRepository = employeeRepository;
+        this.emailService = emailService;
     }
 
     public void sendGreetings(LocalDateTime today) {
---
+++ src/test/java/BirthdayServiceTest.java
@@ -0,0 +1,74 @@
+import com.filnik.repository.EmailAddress;
+import com.filnik.repository.EmployeeRepository;
+import com.filnik.service.BirthdayService;
+import com.filnik.service.EmailService;
+import com.googlecode.gmail4j.GmailClient;
+import com.googlecode.gmail4j.GmailMessage;
+import com.googlecode.gmail4j.javamail.ImapGmailClient;
+import com.googlecode.gmail4j.javamail.ImapGmailConnection;
+import com.googlecode.gmail4j.javamail.JavaMailGmailMessage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnit4.class)
+public class BirthdayServiceTest {
+
+    @Test
+    public void sendGreetingsToTheRightEmployee() throws Exception {
+        EmployeeRepository employeeRepository = new EmployeeRepositorySpy();
+        EmailServiceSpy mailService = new EmailServiceSpy();
+        BirthdayService birthdayService = new BirthdayService(employeeRepository, mailService);
+        birthdayService.sendGreetings(LocalDateTime.now());
+
+        assertTrue(mailService.MESSAGE.getSubject().equals("Happy birthday!"));
+        assertTrue(mailService.MESSAGE.getContentText().equals("Happy birthday, dear John!"));
+    }
+
+    private class EmployeeRepositorySpy extends EmployeeRepository{
+        @Override
+        public EmailAddress[] loadFromDatabase() {
+            return super.loadFromDatabase();
+        }
+
+        @Override
+        public EmailAddress[] load() {
+            return new EmailAddress[]{};
+        }
+
+        @Override
+        public void store(EmailAddress... emailAddress) {
+
+        }
+
+        @Override
+        public void delete(EmailAddress... address) {
+
+        }
+    }
+
+    private class EmailServiceSpy extends EmailService {
+        public JavaMailGmailMessage MESSAGE;
+
+        public EmailServiceSpy() {
+
+        }
+
+        @Override
+        public void send(String email, String subject, String content, String contentType) throws Exception {
+            MESSAGE = new JavaMailGmailMessage();
+            MESSAGE.addTo(new com.googlecode.gmail4j.EmailAddress(email));
+            MESSAGE.setSubject(subject);
+            MESSAGE.setContentText(content);
+        }
+
+        @Override
+        public void finalize() throws Throwable {
+        }
+    }
+}
